1308de97abb089105feac0a88d6a147c5fab99f7
Ian <ianmonteiromoreira@gmail.com>
2024-05-07 11:46:09 -0300

    finalizar ex5

---
+++ ex5.py
@@ -0,0 +1,39 @@
+"""
+5. Preencha uma matriz 5x4 com números informados pelo usuário e exiba a matriz. Em
+seguida, solicite um número e faça uma busca na matriz por esse número, informando a
+posição onde ele se encontra (índice da linha e índice da coluna). Se o número aparecer mais
+de uma vez na matriz, exiba todas as posições onde ele foi encontrado.
+"""
+
+from random import randint
+
+matriz, vezes, posicao, pos = [], [], [], []
+
+for i in range(5):
+    linha = []
+    for j in range(4):
+        n = randint(1, 20)
+       # n = int(input(f'Posição [{i}, {j}]: '))
+        linha.append(n)
+    matriz.append(linha)
+
+for i in range(len(matriz)):
+    for j in range(len(matriz[0])):
+        print(matriz[i][j], end='\t')
+    print()
+
+num = int(input('Número: '))
+
+for i in range(5):
+    posicao.append(pos)
+    for j in range(4):
+        pos = []
+        if matriz[i][j] == num:
+            vezes.append(matriz[i][j])
+            pos.append(i)
+            pos.append(j)
+
+
+
+
+print(f'O número {num} foi encontrado {len(vezes)} vezes, nas posições {pos}')
